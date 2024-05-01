bd4313138c046f68baad47109f03ae8c8ea72790
banzanic <Lukasz.szczepaniak@student.uj.edu.pl>
2024-05-01 14:24:23 +0200

    changes to functionality and submitController

--- src/main/java/com/example/budgetmanagement/controller/SubmitController.java
+++ src/main/java/com/example/budgetmanagement/controller/SubmitController.java
@@ -18,12 +18,12 @@ public class SubmitController {
     @PostMapping("/submitExpenses")
     public void submitExpenses(@ModelAttribute("expensesModel") ExpensesModel expensesModel) {
         System.out.println(expensesModel);
-        System.out.println("Groceries: "+expensesModel.getGroceries()+", Rent: "+expensesModel.getRent()+", Transportation: "+expensesModel.getTransportation()+", Subscriptions: "+expensesModel.getSubscriptions()+", Health care: "+expensesModel.getHealthCare()+", Entertainment: "+expensesModel.getEntertainment()+", Debt: "+expensesModel.getDebt());
+        System.out.println("Groceries: " + expensesModel.getGroceries() + ", Rent: " + expensesModel.getRent() + ", Transportation: " + expensesModel.getTransportation() + ", Subscriptions: " + expensesModel.getSubscriptions() + ", Health care: " + expensesModel.getHealthCare() + ", Entertainment: " + expensesModel.getEntertainment() + ", Debt: " + expensesModel.getDebt());
     }
 
     @PostMapping("/submitIncome")
     public void submitIncome(@ModelAttribute("incomeModel") IncomeModel incomeModel) {
         System.out.println(incomeModel);
-        System.out.println("Groceries: "+incomeModel.getSalary()+", Investment: "+incomeModel.getInvestment()+", Gift: "+incomeModel.getGift()+", Interest: "+incomeModel.getInterest()+", Rental: "+incomeModel.getRental()+", Sales: "+incomeModel.getSales());
+        System.out.println("Groceries: " + incomeModel.getSalary() + ", Investment: " + incomeModel.getInvestment() + ", Gift: " + incomeModel.getGift() + ", Interest: " + incomeModel.getInterest() + ", Rental: " + incomeModel.getRental() + ", Sales: " + incomeModel.getSales());
     }
 }
