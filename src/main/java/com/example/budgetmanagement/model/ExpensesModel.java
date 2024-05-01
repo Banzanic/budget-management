9b85c789347018d0a3e60e29943165bcb65a8462
karolinawegrzyn <karolinaa.wegrzyn@student.uj.edu.pl>
2024-05-01 15:57:58 +0200

    Add lombok dependency, use Getter and Setter annotation

--- src/main/java/com/example/budgetmanagement/model/ExpensesModel.java
+++ src/main/java/com/example/budgetmanagement/model/ExpensesModel.java
@@ -1,5 +1,10 @@
 package com.example.budgetmanagement.model;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class ExpensesModel {
     private Integer groceries;
     private Integer rent;
@@ -9,59 +14,4 @@ public class ExpensesModel {
     private Integer entertainment;
     private Integer debt;
 
-    public Integer getGroceries() {
-        return groceries;
-    }
-
-    public void setGroceries(Integer groceries) {
-        this.groceries = groceries;
-    }
-
-    public Integer getRent() {
-        return rent;
-    }
-
-    public void setRent(Integer rent) {
-        this.rent = rent;
-    }
-
-    public Integer getTransportation() {
-        return transportation;
-    }
-
-    public void setTransportation(Integer transportation) {
-        this.transportation = transportation;
-    }
-
-    public Integer getSubscriptions() {
-        return subscriptions;
-    }
-
-    public void setSubscriptions(Integer subscriptions) {
-        this.subscriptions = subscriptions;
-    }
-
-    public Integer getHealthCare() {
-        return healthCare;
-    }
-
-    public void setHealthCare(Integer healthCare) {
-        this.healthCare = healthCare;
-    }
-
-    public Integer getEntertainment() {
-        return entertainment;
-    }
-
-    public void setEntertainment(Integer entertainment) {
-        this.entertainment = entertainment;
-    }
-
-    public Integer getDebt() {
-        return debt;
-    }
-
-    public void setDebt(Integer debt) {
-        this.debt = debt;
-    }
 }
