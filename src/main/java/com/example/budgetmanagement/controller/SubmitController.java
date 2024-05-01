367caa1acff537c947e626d62306479048db2d83
banzanic <Lukasz.szczepaniak@student.uj.edu.pl>
2024-05-01 19:58:34 +0200

    SubmitController for the view to stay the same

--- src/main/java/com/example/budgetmanagement/controller/SubmitController.java
+++ src/main/java/com/example/budgetmanagement/controller/SubmitController.java
@@ -2,23 +2,31 @@ package com.example.budgetmanagement.controller;
 
 import com.example.budgetmanagement.model.ExpensesModel;
 import com.example.budgetmanagement.model.IncomeModel;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class SubmitController {
 
     @PostMapping("/submitExpenses")
-    public void submitExpenses(@ModelAttribute("expensesModel") ExpensesModel expensesModel) {
+    public String submitExpenses(@ModelAttribute("expensesModel") ExpensesModel expensesModel, Model model) {
+        model.addAttribute("incomeModel", new IncomeModel());
         System.out.println(expensesModel);
         System.out.println("Groceries: " + expensesModel.getGroceries() + ", Rent: " + expensesModel.getRent() + ", Transportation: " + expensesModel.getTransportation() + ", Subscriptions: " + expensesModel.getSubscriptions() + ", Health care: " + expensesModel.getHealthCare() + ", Entertainment: " + expensesModel.getEntertainment() + ", Debt: " + expensesModel.getDebt() + ", Year: " + expensesModel.getYear() + ", Month: " + expensesModel.getMonth());
+
+        return "functionality";
 
     }
 
     @PostMapping("/submitIncome")
-    public void submitIncome(@ModelAttribute("incomeModel") IncomeModel incomeModel) {
+    public String submitIncome(@ModelAttribute("incomeModel") IncomeModel incomeModel, Model model) {
+        model.addAttribute("expensesModel", new ExpensesModel());
         System.out.println(incomeModel);
         System.out.println("Groceries: " + incomeModel.getSalary() + ", Investment: " + incomeModel.getInvestment() + ", Gift: " + incomeModel.getGift() + ", Interest: " + incomeModel.getInterest() + ", Rental: " + incomeModel.getRental() + ", Sales: " + incomeModel.getSales() + ", Year: " + incomeModel.getYear() + ", Month: " + incomeModel.getMonth());
+
+        return "functionality";
     }
 }
